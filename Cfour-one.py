d814da7d17cbd3c8b77b7860b3f83ea547c1eeea
12agarwala <microversepro@gmail.com>
2021-07-19 18:33:06 +0800

    committing

---
+++ Cfour-one.py
@@ -0,0 +1,4 @@
+pizzas = ['BBQ and Mushroom', 'Trio cheese', 'Margherita']
+for pizza in pizzas:
+    print("I like", pizza, "pizza")
+print("I really enjoy eating pizza because of its unique texture. Each pizza has a different touch to it. My top three pizzas that I crave for are Margherita, BBQ and Mushroom, and Trio Cheese.")
