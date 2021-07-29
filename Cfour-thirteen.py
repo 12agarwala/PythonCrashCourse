bd37fb92c21c08b23ac049e0735a05fe36babbe9
12agarwala <microversepro@gmail.com>
2021-07-29 14:56:34 +0800

    committing

---
+++ Cfour-thirteen.py
@@ -0,0 +1,8 @@
+foods = ("pizzas", "pastas", "burgers", "noodles", "sandwiches")
+print("Original food items:")
+for food in foods:
+    print(food)
+foods = ("pizzas", "pastas", "burgers", "dumplings", "fried rice")
+print("\nModified food items:")
+for food in foods:
+    print(food)
