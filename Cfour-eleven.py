61b181eb8fcfe6beb5ef8ee8128894f117f6aaa4
12agarwala <microversepro@gmail.com>
2021-07-29 11:10:48 +0800

    committing

---
+++ Cfour-eleven.py
@@ -0,0 +1,9 @@
+pizzas = ['BBQ and Mushroom', 'Trio cheese', 'Margherita', 'Pepperoni']
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza, "pizza")
+print("I really enjoy eating pizza because of its unique texture. Each pizza has a different touch to it. My top three pizzas that I crave for are Margherita, BBQ and Mushroom, and Trio Cheese.")
+friend_pizzas = ['BBQ and Mushroom', 'Trio cheese', 'Margherita', 'Veggie']
+print("My friend's favorite pizzas are:")
+for friend_pizza in friend_pizzas:
+    print(friend_pizza, "pizza")
