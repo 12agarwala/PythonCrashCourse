f85939c1104d5dccfbd40fc27b84aea072dde8ef
12agarwala <microversepro@gmail.com>
2021-08-10 13:37:49 +0800

    committing

---
+++ C5-3.py
@@ -0,0 +1,4 @@
+alien_color = ["red", "yellow", "green"]
+player = input("Please enter alien color: ")
+if player == alien_color:
+    print("You are correct. You have earned 5 points!")
---
+++ C5-4.py
@@ -0,0 +1,7 @@
+alien_color = ("green")
+player = input("Please enter alien color: ")
+if player == alien_color:
+    print("You are correct. You have earned 5 points!")
+else:
+    print("You have earned 10 points!")
+
