73972dc80066c038e208c7358fe9e39d32dffea8
12agarwala <microversepro@gmail.com>
2021-08-13 10:29:17 +0800

    committing

--- C5-8.py
+++ C5-8.py
@@ -1,6 +1,7 @@
 usernames = ['admin', 'BigNoseMan444', '420_HBirthday', 'SnotFace42069', 'FartFace222']
 username = input("Please enter your username to login: ")
-if username == 'admin':
-    print("Hello admin, would you like to see a status report?")
-else:
-    print("Hello", username, ", thank you for logging in again.")
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello", username, ", thank you for logging in again.")
