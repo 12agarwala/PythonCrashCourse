f2f93b948af523b8ad7efdc64db876c134e9f69a
12agarwala <microversepro@gmail.com>
2021-08-14 14:41:57 +0800

    committing

--- C5-6.py
+++ C5-6.py
@@ -1,14 +1,12 @@
 person = int(input("Please enter your age only in whole numbers. For example, 11, not 11 years and 4 four months. : "))
 if person < 2:
     print("You are a baby")
-elif person >= 2:
+elif person <4:
     print("You are a toddler")
-elif person >= 4:
+elif person < 13:
     print("You are a kid")
-elif person >= 13:
+elif person < 20:
     print("You are a teenager")
-elif person >= 20:
-    print("You are an adult")
 elif person < 65:
     print("You are an adult")
 else:
