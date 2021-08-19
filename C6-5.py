e9613d57246ddb39f2512972b680cf9ff2de2286
12agarwala <microversepro@gmail.com>
2021-08-19 10:42:41 +0800

    committing

---
+++ C6-5.py
@@ -0,0 +1,6 @@
+rivers = {"Brahmaputra": "India", "Ganges": "India", "Indus": "India"}
+for name in sorted(rivers.keys()):
+    print(f" The {name.title()}, is in India.")
+for river in rivers:
+    print(river)
+print("India")
