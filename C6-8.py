4139e2f5a2879dc41d18bdc51ab407786c6eba9d
12agarwala <microversepro@gmail.com>
2021-08-19 13:13:11 +0800

    committing

---
+++ C6-8.py
@@ -0,0 +1,6 @@
+animals1 = {"bird": 'George', "snake": 'Bob', "dog": 'Akshat'}
+animals2 = {"cat": 'Thomas', "guinea pig": 'Jerry', "rabbit": 'Richard'}
+animals3 = {"fish": 'Tom', "turtle": 'John', "hamster": 'David'}
+pets = [animals1, animals2, animals3]
+for pet in pets:
+    print(pet)
