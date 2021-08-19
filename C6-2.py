dc4a661d8094ad8f56fffade95f98affa31a95d1
12agarwala <microversepro@gmail.com>
2021-08-19 13:16:56 +0800

    committing

--- C6-2.py
+++ C6-2.py
@@ -1,2 +1,2 @@
 names = {"Bob": 2, "Blob": 4, "James": 6, "Thomas": 8, "Richard": 7}
-print("Bob", names["Bob"], "Blob", names["Blob"], "James", names["James"], "Thomas", names["Thomas"], "Richard", names["Richard"],)
+print("Bob:", names["Bob"], "Blob:", names["Blob"], "James:", names["James"], "Thomas:", names["Thomas"], "Richard:", names["Richard"],)
