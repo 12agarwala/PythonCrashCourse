a269d05f8d873bdb819395ebf09a12d9e43b0099
12agarwala <microversepro@gmail.com>
2021-08-19 13:20:34 +0800

    committing

---
+++ C6-10.py
@@ -0,0 +1,2 @@
+names = {"Bob": '2 and 3,', "Blob": '4 and 0,', "James": '6 and 1,', "Thomas": '8 and 5,', "Richard": '7 and 9'}
+print("Bob:", names["Bob"], "Blob:", names["Blob"], "James:", names["James"], "Thomas:", names["Thomas"], "Richard:", names["Richard"],)
