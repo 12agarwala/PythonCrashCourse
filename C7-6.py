cedadc5ca49d70d3586471074363ce63e9c4f073
12agarwala <microversepro@gmail.com>
2021-08-20 16:52:34 +0800

    committing

---
+++ C7-6.py
@@ -0,0 +1,13 @@
+i = 1
+while i == 1:
+    toppings = input("Please enter the toppings for your pizza: ")
+    if toppings == 'quit':
+        i = 6
+    else:
+        print("I will add", toppings, "to the list")
+        print("If your are done, type quit")
+
+toppings = 1
+while toppings != 'quit':
+    toppings = input("Please enter the toppings for your pizza: ")
+    print("I will add", toppings, "to the list. Or else type quit")
