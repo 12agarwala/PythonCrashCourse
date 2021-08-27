4808ff54704e4489f1b5a9f2b300896588a6bd0b
12agarwala <microversepro@gmail.com>
2021-08-27 16:50:33 +0800

    committing

--- C7-9.py
+++ C7-9.py
@@ -1,8 +1,13 @@
-print("We have run out of pastrami")
+print("We have run out of Pastrami")
 sandwich_orders = ["Cheese", "Pastrami", "Vegetable", "Pastrami", "Nutella", "Peanut Butter", "Pastrami", "PBJ",  "Pastrami","Grilled Cheese"]
-while 'pastrami' in sandwich_orders:
-    sandwich_orders.remove('pastrami')
-for sandwich_order in sandwich_orders:
-        print("I made your", sandwich_order, "sandwich")
 finished_sandwiches = []
+i = sandwich_orders.count("Pastrami")
+while i > 0:
+    sandwich_orders.remove("Pastrami")
+    i = i-1
+for sandwich_order in sandwich_orders:
+    print("I made your", sandwich_order, "sandwich")
+    finished_sandwiches.append(sandwich_order)
+print("I have finished making all your sandwiches", finished_sandwiches)
+
 
