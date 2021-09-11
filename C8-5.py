8ae503cd2dd673b3611360e1f66161a2cf06984c
12agarwala <microversepro@gmail.com>
2021-09-11 14:40:51 +0800

    committing

--- C8-5.py
+++ C8-5.py
@@ -1,8 +1,6 @@
-def describe_city(City, Country):
-    if Country == "":
-        Country = 'China'
+def describe_city(City, Country = 'China'):
     print(f"{City.title()} is in {Country.title()}")
 
 describe_city('London', 'England')
-describe_city('Cairo', '')
-describe_city('Paris', '')
+describe_city('Cairo')
+describe_city('Paris')
