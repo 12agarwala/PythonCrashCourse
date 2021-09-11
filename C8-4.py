dc82718be6fdf9d5a3dde737d70601ca956e448a
12agarwala <microversepro@gmail.com>
2021-09-11 14:20:08 +0800

    committing

--- C8-4.py
+++ C8-4.py
@@ -1,8 +1,8 @@
 def make_shirt(size, message):
-    print("The shirt is:", str(size), "and has a message on it saying:", str(message))
+    print("The shirt size is:", size, "and has a message on it saying:", message)
 
-S = str(input("Please enter the size of your shirt (default size is large)"))
-M = str(input("Please enter the message you want on your shirt (default text is: 'I Love Python')"))
+S = input("Please enter the size of your shirt (default size is large)")
+M = input("Please enter the message you want on your shirt (default text is: 'I Love Python')")
 if S == '':
     S = 'large'
 if M == '':
