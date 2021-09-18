0d4d281f3d979843c2f7b0bb80844786be755749
12agarwala <microversepro@gmail.com>
2021-09-18 14:46:49 +0800

    committing

--- C8-7.py
+++ C8-7.py
@@ -1,10 +1,11 @@
-def make_album(Artist_name, Album_title):
-    Dict = {'name': Artist_name, 'album': Album_title}
+def make_album(Artist_name, Album_title, Songs = ''):
+    Dict = {'name': Artist_name, 'album': Album_title, '# of songs': Songs}
     return Dict
 for i in range(3):
     Artist_name = input("Please enter the name of an artist: ")
     Album_title = input("Please enter the name of one of the artist's albums: ")
-    x = make_album(Artist_name, Album_title)
+    Songs = input("Please enter the total number of song in the album: ")
+    x = make_album(Artist_name, Album_title, Songs)
     print(x)
 
 
