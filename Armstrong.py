4752218aaf67a0ac7322410b96baa1a8c881c6e9
12agarwala <microversepro@gmail.com>
2021-10-04 09:37:28 +0800

    committing

---
+++ Armstrong.py
@@ -0,0 +1,2 @@
+1^3+2^3+3^3=1+8+27=36
+#Based on the number of digits entered, raise it to the power of that number. eg. 3 digits rasied to 3rd power, 4 digits raised to 4th power, etc. Each digit will be raised to the power. Then add the values together.
---
+++ C8-12.py
@@ -0,0 +1,14 @@
+def sandwich_toppings (type, size, *toppings):
+    print("Your sandwich type is", type, 'the bread size is', size, 'and the following toppings')
+    print(toppings)
+#main program start here
+bread_type = input("Please enter what type of bread you want for your sandwich: ")
+bread_size = input("Please enter your prefered bread size: ")
+toppings = []
+while True:
+    top = input("Please enter sandwich toppings (enter done if finished): ")
+    if top.lower() != 'done':
+        toppings.append (top)
+    else:
+        break
+sandwich_toppings(bread_type, bread_size, toppings)
