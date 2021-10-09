38e29585d27d794fd8fb70cf99a0f522e375bbc7
12agarwala <microversepro@gmail.com>
2021-10-09 14:41:29 +0800

    committing

--- Armstrong.py
+++ Armstrong.py
@@ -1,2 +1,20 @@
-1^3+2^3+3^3=1+8+27=36
-#Based on the number of digits entered, raise it to the power of that number. eg. 3 digits rasied to 3rd power, 4 digits raised to 4th power, etc. Each digit will be raised to the power. Then add the values together.
+#1^3+2^3+3^3=1+8+27=36
+#Based on the number of digits entered, raise it to the power of that number. eg. 3 digits rasied to 3rd power, 4 digits raised to 4th power, etc. Each digit will be raised to the power. Then add the values together
+
+#numbers = []
+#result = ()
+#while True:
+#    number = input("Please enter a number (enter done if finished): ")
+#    if number != 'done':
+#        numbers.append(number)
+#    else:
+#        break
+number = input("Please enter a number: ")
+x = len(number)
+result = 0
+for i in number:
+    result = result + int(i) ** x
+if result == int(number):
+    print("It's armstrong number")
+else:
+    print("It's not armstrong number")
