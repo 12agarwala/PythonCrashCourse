21fbd51a80aea95a67b35bfc6c8334bfc2ac1ae1
12agarwala <microversepro@gmail.com>
2021-10-22 02:16:40 +0800

    committing

--- Bus Program extended.py
+++ Bus Program extended.py
@@ -5,15 +5,31 @@ busD = []
 busE = []
 busF = []
 
-userInputBus = 'x'
+userInputBus = ''
 
 
 def bus():
     global userInputBus
+    userInputBus = input("Please enter the correct bus letter: ")
     while userInputBus.lower() != 'a' and userInputBus.lower() != 'b' and userInputBus.lower() != 'c' and userInputBus.lower() != 'd' and userInputBus.lower() != 'e' and userInputBus.lower() != 'f':
         print('Invalid input! Please enter the correct bus letter.')
         userInputBus = input("Please enter the correct bus letter: ")
     print("Valid input entered.")
+
+
+def weekChecker():
+    if userInputBus == 'a':
+        return busA
+    elif userInputBus == 'b':
+        return busB
+    elif userInputBus == 'c':
+        return busB
+    elif userInputBus == 'd':
+        return busB
+    elif userInputBus == 'e':
+        return busB
+    elif userInputBus == 'f':
+        return busB
 
 
 bus()
@@ -21,7 +37,7 @@ bus()
 storedValue = 0
 lateValue = 0
 lateValueCounter = 0
-for i in range(5):
+for i in range(20):
     try:
         inputBusValue = int(input(f"Please enter a number for bus {userInputBus.title()}: "))
     except ValueError:
@@ -41,7 +57,7 @@ for i in range(5):
     if inputBusValue < 0:
         lateValueCounter += 1
         lateValue += inputBusValue
-print(busA)
+print(userInputBus)
 # End of Task 1
 
 forBusCounter = 0
@@ -68,42 +84,19 @@ for i in range(6):
         print("Error in forBusCounter function")
     for n in forBusCounter:
         busACount = busACount + n
+        busBCount = busBCount + n
+        busCCount = busCCount + n
+        busDCount = busDCount + n
+        busECount = busECount + n
+        busFCount = busFCount + n
 
-busAAverage = busACount / len(busA)
-if lateValueCounter != 0:
-    busALateAverage = lateValue / lateValueCounter
-    print(f"Bus A late average is {round(busALateAverage)} minutes.")
-print(f"Bus A average is {round(busAAverage)} minutes.")
+busAverage = busACount / len(weekChecker())
 
-busBAverage = busBCount / len(busB)
 if lateValueCounter != 0:
-    busBLateAverage = lateValue / lateValueCounter
-    print(f"Bus B late average is {round(busBLateAverage)} minutes.")
-print(f"Bus B average is {round(busBAverage)} minutes.")
+    busLateAverage = lateValue / lateValueCounter
+    print(f"Bus {userInputBus.title()} late average is {round(busLateAverage)} minutes.")
+print(f"Bus {userInputBus.title()} average is {round(busAverage)} minutes.")
 
-busCAverage = busCCount / len(busC)
-if lateValueCounter != 0:
-    busCLateAverage = lateValue / lateValueCounter
-    print(f"Bus C late average is {round(busCLateAverage)} minutes.")
-print(f"Bus C average is {round(busCAverage)} minutes.")
-
-busDAverage = busDCount / len(busD)
-if lateValueCounter != 0:
-    busDLateAverage = lateValue / lateValueCounter
-    print(f"Bus D late average is {round(busDLateAverage)} minutes.")
-print(f"Bus D average is {round(busDAverage)} minutes.")
-
-busEAverage = busECount / len(busE)
-if lateValueCounter != 0:
-    busELateAverage = lateValue / lateValueCounter
-    print(f"Bus E late average is {round(busELateAverage)} minutes.")
-print(f"Bus E average is {round(busEAverage)} minutes.")
-
-busFAverage = busFCount / len(busF)
-if lateValueCounter != 0:
-    busFLateAverage = lateValue / lateValueCounter
-    print(f"Bus F late average is {round(busFLateAverage)} minutes.")
-print(f"Bus F average is {round(busFAverage)} minutes.")
 
 def day():
     internalDay = input('Please enter the day (e.g. mon, thu):')
@@ -116,93 +109,19 @@ def day():
 
 def userDayChecker():
     userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busA[0] < 0:
-        print(f"Bus A was {busA[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busA[1] < 0:
-        print(f"Bus A was {busA[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busA[2] < 0:
-        print(f"Bus A was {busA[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busA[3] < 0:
-        print(f"Bus A was {busA[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busA[4] < 0:
-        print(f"Bus A was {busA[4] * -1} minutes late on Friday.")
+    if userDayCheckerDay.lower() == 'mon' and weekChecker()[0] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[0] * -1} minutes late on Monday.")
+    elif userDayCheckerDay.lower() == 'tue' and weekChecker()[1] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[1] * -1} minutes late on Tuesday.")
+    elif userDayCheckerDay.lower() == 'wed' and weekChecker()[2] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[2] * -1} minutes late on Wednesday.")
+    elif userDayCheckerDay.lower() == 'thu' and weekChecker()[3] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[3] * -1} minutes late on Thursday.")
+    elif userDayCheckerDay.lower() == 'fri' and weekChecker()[4] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[4] * -1} minutes late on Friday.")
     else:
-        print(f"Bus A was not late on {userDayCheckerDay.title()}!")
+        print(f"Bus {userInputBus.title()} was not late on {userDayCheckerDay.title()}!")
 
-def userDayChecker():
-    userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busB[0] < 0:
-        print(f"Bus B was {busB[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busB[1] < 0:
-        print(f"Bus B was {busB[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busB[2] < 0:
-        print(f"Bus B was {busB[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busB[3] < 0:
-        print(f"Bus B was {busB[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busB[4] < 0:
-        print(f"Bus B was {busB[4] * -1} minutes late on Friday.")
-    else:
-        print(f"Bus B was not late on {userDayCheckerDay.title()}!")
-
-def userDayChecker():
-    userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busC[0] < 0:
-        print(f"Bus C was {busC[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busC[1] < 0:
-        print(f"Bus C was {busC[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busC[2] < 0:
-        print(f"Bus C was {busC[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busC[3] < 0:
-        print(f"Bus C was {busC[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busC[4] < 0:
-        print(f"Bus C was {busC[4] * -1} minutes late on Friday.")
-    else:
-        print(f"Bus C was not late on {userDayCheckerDay.title()}!")
-
-def userDayChecker():
-    userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busD[0] < 0:
-        print(f"Bus D was {busD[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busD[1] < 0:
-        print(f"Bus D was {busD[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busD[2] < 0:
-        print(f"Bus D was {busD[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busD[3] < 0:
-        print(f"Bus D was {busD[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busD[4] < 0:
-        print(f"Bus D was {busD[4] * -1} minutes late on Friday.")
-    else:
-        print(f"Bus D was not late on {userDayCheckerDay.title()}!")
-
-def userDayChecker():
-    userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busE[0] < 0:
-        print(f"Bus E was {busE[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busE[1] < 0:
-        print(f"Bus E was {busE[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busE[2] < 0:
-        print(f"Bus E was {busE[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busE[3] < 0:
-        print(f"Bus E was {busE[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busE[4] < 0:
-        print(f"Bus E was {busE[4] * -1} minutes late on Friday.")
-    else:
-        print(f"Bus E was not late on {userDayCheckerDay.title()}!")
-
-def userDayChecker():
-    userDayCheckerDay = day()
-    if userDayCheckerDay.lower() == 'mon' and busF[0] < 0:
-        print(f"Bus F was {busF[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay.lower() == 'tue' and busF[1] < 0:
-        print(f"Bus F was {busF[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay.lower() == 'wed' and busF[2] < 0:
-        print(f"Bus F was {busF[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay.lower() == 'thu' and busF[3] < 0:
-        print(f"Bus F was {busF[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay.lower() == 'fri' and busF[4] < 0:
-        print(f"Bus F was {busF[4] * -1} minutes late on Friday.")
-    else:
-        print(f"Bus F was not late on {userDayCheckerDay.title()}!")
 
 userDayChecker()
 userChecker = input(f"Would you like to input another value (y/n)? ")
--- Bus Program.py
+++ Bus Program.py
@@ -5,15 +5,31 @@ busD = []
 busE = []
 busF = []
 
-userInputBus = 'x'
+userInputBus = ''
 
 
 def bus():
     global userInputBus
+    userInputBus = input("Please enter the correct bus letter: ")
     while userInputBus.lower() != 'a' and userInputBus.lower() != 'b' and userInputBus.lower() != 'c' and userInputBus.lower() != 'd' and userInputBus.lower() != 'e' and userInputBus.lower() != 'f':
         print('Invalid input! Please enter the correct bus letter.')
         userInputBus = input("Please enter the correct bus letter: ")
     print("Valid input entered.")
+
+
+def weekChecker():
+    if userInputBus == 'a':
+        return busA
+    elif userInputBus == 'b':
+        return busB
+    elif userInputBus == 'c':
+        return busB
+    elif userInputBus == 'd':
+        return busB
+    elif userInputBus == 'e':
+        return busB
+    elif userInputBus == 'f':
+        return busB
 
 
 bus()
@@ -21,7 +37,7 @@ bus()
 storedValue = 0
 lateValue = 0
 lateValueCounter = 0
-for i in range(5):
+for i in range(20):
     try:
         inputBusValue = int(input(f"Please enter a number for bus {userInputBus.title()}: "))
     except ValueError:
@@ -41,7 +57,7 @@ for i in range(5):
     if inputBusValue < 0:
         lateValueCounter += 1
         lateValue += inputBusValue
-print(busA)
+print(userInputBus)
 # End of Task 1
 
 forBusCounter = 0
@@ -68,12 +84,18 @@ for i in range(6):
         print("Error in forBusCounter function")
     for n in forBusCounter:
         busACount = busACount + n
+        busBCount = busBCount + n
+        busCCount = busCCount + n
+        busDCount = busDCount + n
+        busECount = busECount + n
+        busFCount = busFCount + n
 
-busAAverage = busACount / len(busA)
+busAverage = busACount / len(weekChecker())
+
 if lateValueCounter != 0:
-    busALateAverage = lateValue / lateValueCounter
-    print(f"Bus A late average is {round(busALateAverage)} minutes.")
-print(f"Bus A average is {round(busAAverage)} minutes.")
+    busLateAverage = lateValue / lateValueCounter
+    print(f"Bus {userInputBus.title()} late average is {round(busLateAverage)} minutes.")
+print(f"Bus {userInputBus.title()} average is {round(busAverage)} minutes.")
 
 
 def day():
@@ -87,18 +109,18 @@ def day():
 
 def userDayChecker():
     userDayCheckerDay = day()
-    if userDayCheckerDay == 'mon' and busA[0] < 0:
-        print(f"Bus A was {busA[0] * -1} minutes late on Monday.")
-    elif userDayCheckerDay == 'tue' and busA[1] < 0:
-        print(f"Bus A was {busA[1] * -1} minutes late on Tuesday.")
-    elif userDayCheckerDay == 'wed' and busA[2] < 0:
-        print(f"Bus A was {busA[2] * -1} minutes late on Wednesday.")
-    elif userDayCheckerDay == 'thu' and busA[3] < 0:
-        print(f"Bus A was {busA[3] * -1} minutes late on Thursday.")
-    elif userDayCheckerDay == 'fri' and busA[4] < 0:
-        print(f"Bus A was {busA[4] * -1} minutes late on Friday.")
+    if userDayCheckerDay.lower() == 'mon' and weekChecker()[0] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[0] * -1} minutes late on Monday.")
+    elif userDayCheckerDay.lower() == 'tue' and weekChecker()[1] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[1] * -1} minutes late on Tuesday.")
+    elif userDayCheckerDay.lower() == 'wed' and weekChecker()[2] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[2] * -1} minutes late on Wednesday.")
+    elif userDayCheckerDay.lower() == 'thu' and weekChecker()[3] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[3] * -1} minutes late on Thursday.")
+    elif userDayCheckerDay.lower() == 'fri' and weekChecker()[4] < 0:
+        print(f"Bus {userInputBus.title()} was {weekChecker()[4] * -1} minutes late on Friday.")
     else:
-        print(f"Bus A was not late on {userDayCheckerDay.title()}!")
+        print(f"Bus {userInputBus.title()} was not late on {userDayCheckerDay.title()}!")
 
 
 userDayChecker()
