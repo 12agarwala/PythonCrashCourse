6d09516820490e8b9f9b06b7c12b52c7e25d8464
12agarwala <microversepro@gmail.com>
2022-03-08 08:34:43 +0800

    commit

--- singleTickets.py
+++ singleTickets.py
@@ -1,21 +1,22 @@
-def singleTicket():
+def singleTickets():
     numberTickets = [0, 0, 0]
     while True:
         singleTicketType = input("What type of single tickets do you want? ")
         if singleTicketType.lower() == "adult" or singleTicketType.lower() == "senior" or singleTicketType.lower() == "child":
             break
         else:
-            print("Please enter the correct ticket type: ")
+            print("Please enter the correct ticket type")
     while True:
-        ticketNumber = int(input("How many", singleTicketType, "tickets do you want? "))
+        ticketNumber = input("How many " + singleTicketType + " tickets do you want? ")
         if ticketNumber.isnumeric():
+            ticketNumber = int(ticketNumber)
             break
         else:
-            print("Please enter the correct number: ")
+            print("Please enter the correct number")
     if singleTicketType == "adult":
         numberTickets[0] = numberTickets[0] + ticketNumber
     elif singleTicketType == "senior":
         numberTickets[1] = numberTickets[1] + ticketNumber
     elif singleTicketType == "child":
         numberTickets[2] = numberTickets[2] + ticketNumber
-    return
+    return numberTickets
